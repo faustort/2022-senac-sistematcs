aa8b0fc716961948e072737aece4605c8c0c1bd4
Fausto <faustortoloi@gmail.com>
2022-11-11 19:56:16 -0300

    Config

--- lib/config.php
+++ lib/config.php
@@ -1,3 +1,10 @@
-<?php 
+<?php
 
-?>
+// Definir a variável da raiz do site
+define("URL_BASE", "http://localhost/sistematcs/");
+
+// Definir as variáveis de configuração do Banco Mysql
+define("DB_HOST", "localhost");
+define("DB_USER", 'root');
+define("DB_PASSWORD", '');
+define("DB_NAME", 'sistema_tcs');
