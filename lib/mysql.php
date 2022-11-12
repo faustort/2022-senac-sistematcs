c5aa4a07a1e295b76fd92aee5d6aefad9d0bcb25
Fausto <faustortoloi@gmail.com>
2022-11-11 21:04:45 -0300

    Conexão

--- lib/mysql.php
+++ lib/mysql.php
@@ -1,2 +1,11 @@
 <?php
-include_once "config.php";
+include_once __DIR__ . "/config.php";
+
+// Conectar ao banco de dados usando PDO
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
+}
+
